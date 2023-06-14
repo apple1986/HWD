c29741ceee7602cdfa93a7ce396d600bbf8b1de1
apple1986 <xugp2006@126.com>
2023-06-14 14:57:54 +0800

    Add files via upload

---
+++ networks/deepv3.py
@@ -0,0 +1,364 @@
+
+import logging
+import torch
+from torch import nn
+import sys
+import os
+
+# sys.path.append('..')
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+
+from networks import resnet_d as Resnet_Deep
+from networks.wider_resnet import wider_resnet38_a2
+from networks.nn.mynn import initialize_weights, Upsample
+from networks.nn.operators import conv_bn_relu, conv_sigmoid, _AtrousSpatialPyramidPoolingModule
+
+
+class DeepFCN(nn.Module):
+    """
+    Implement DeepLabV3 model
+    A: stride8
+    B: stride16
+    with skip connections
+    """
+
+    def __init__(self, num_classes, trunk='seresnext-50', criterion=None, variant='D',
+                 skip='m1', skip_num=48):
+        super(DeepFCN, self).__init__()
+        self.criterion = criterion
+        self.variant = variant
+        self.skip = skip
+        self.skip_num = skip_num
+
+        if trunk == 'resnet-50-deep':
+            resnet = Resnet_Deep.resnet50()
+            resnet.layer0 = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool)
+        elif trunk == 'resnet-101-deep':
+            resnet = Resnet_Deep.resnet101()
+            resnet.layer0 = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool)
+        else:
+            raise ValueError("Not a valid network arch")
+
+        self.layer0 = resnet.layer0
+        self.layer1, self.layer2, self.layer3, self.layer4 = \
+            resnet.layer1, resnet.layer2, resnet.layer3, resnet.layer4
+
+        if self.variant == 'D':
+            for n, m in self.layer3.named_modules():
+                if 'conv2' in n:
+                    m.dilation, m.padding, m.stride = (2, 2), (2, 2), (1, 1)
+                elif 'downsample.0' in n:
+                    m.stride = (1, 1)
+            for n, m in self.layer4.named_modules():
+                if 'conv2' in n:
+                    m.dilation, m.padding, m.stride = (4, 4), (4, 4), (1, 1)
+                elif 'downsample.0' in n:
+                    m.stride = (1, 1)
+        elif self.variant == 'D16':
+            for n, m in self.layer4.named_modules():
+                if 'conv2' in n:
+                    m.dilation, m.padding, m.stride = (2, 2), (2, 2), (1, 1)
+                elif 'downsample.0' in n:
+                    m.stride = (1, 1)
+        else:
+            print("Not using Dilation ")
+
+        self.fcn_head = nn.Sequential(
+            nn.Conv2d(2048, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+        )
+
+        if self.skip == 'm1':
+            self.bot_fine = nn.Conv2d(256, self.skip_num, kernel_size=1, bias=False)
+        elif self.skip == 'm2':
+            self.bot_fine = nn.Conv2d(512, self.skip_num, kernel_size=1, bias=False)
+        else:
+            raise Exception('Not a valid skip')
+
+
+        self.final = nn.Sequential(
+            nn.Conv2d(256 + self.skip_num, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, num_classes, kernel_size=1, bias=False))
+
+        initialize_weights(self.fcn_head)
+        initialize_weights(self.bot_fine)
+        initialize_weights(self.final)
+
+    def forward(self, x, gts=None):
+
+        x_size = x.size()  # 800
+        x0 = self.layer0(x)  # 400
+        x1 = self.layer1(x0)  # 400
+        x2 = self.layer2(x1)  # 100
+        x3 = self.layer3(x2)  # 100
+        x4 = self.layer4(x3)  # 100
+        dec0_up = self.fcn_head(x4)
+
+        if self.skip == 'm1':
+            dec0_fine = self.bot_fine(x1)
+            dec0_up = Upsample(dec0_up, x1.size()[2:])
+        else:
+            dec0_fine = self.bot_fine(x2)
+            dec0_up = Upsample(dec0_up, x2.size()[2:])
+
+        dec0 = [dec0_fine, dec0_up]
+        dec0 = torch.cat(dec0, 1)
+        dec1 = self.final(dec0)
+        main_out = Upsample(dec1, x_size[2:])
+
+        # if self.training:
+        #     return self.criterion(main_out, gts)
+
+        return main_out
+
+
+class DeepV3Plus(nn.Module):
+    """
+    Implement DeepLabV3 model
+    A: stride8
+    B: stride16
+    with skip connections
+    """
+
+    def __init__(self, num_classes, trunk='seresnext-50', criterion=None, variant='D',
+                 skip='m1', skip_num=48):
+        super(DeepV3Plus, self).__init__()
+        self.criterion = criterion
+        self.variant = variant
+        self.skip = skip
+        self.skip_num = skip_num
+
+        if trunk == 'resnet-50-deep':
+            resnet = Resnet_Deep.resnet50()
+            resnet.layer0 = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool)
+        elif trunk == 'resnet-101-deep':
+            resnet = Resnet_Deep.resnet101()
+            resnet.layer0 = nn.Sequential(resnet.conv1, resnet.bn1, resnet.relu, resnet.maxpool)
+        else:
+            raise ValueError("Not a valid network arch")
+
+        self.layer0 = resnet.layer0
+        self.layer1, self.layer2, self.layer3, self.layer4 = \
+            resnet.layer1, resnet.layer2, resnet.layer3, resnet.layer4
+
+        if self.variant == 'D':
+            for n, m in self.layer3.named_modules():
+                if 'conv2' in n:
+                    m.dilation, m.padding, m.stride = (2, 2), (2, 2), (1, 1)
+                elif 'downsample.0' in n:
+                    m.stride = (1, 1)
+            for n, m in self.layer4.named_modules():
+                if 'conv2' in n:
+                    m.dilation, m.padding, m.stride = (4, 4), (4, 4), (1, 1)
+                elif 'downsample.0' in n:
+                    m.stride = (1, 1)
+        elif self.variant == 'D16':
+            for n, m in self.layer4.named_modules():
+                if 'conv2' in n:
+                    m.dilation, m.padding, m.stride = (2, 2), (2, 2), (1, 1)
+                elif 'downsample.0' in n:
+                    m.stride = (1, 1)
+        else:
+            print("Not using Dilation ")
+
+        self.aspp = _AtrousSpatialPyramidPoolingModule(2048, 256,
+                                                       output_stride=8)
+
+        if self.skip == 'm1':
+            self.bot_fine = nn.Conv2d(256, self.skip_num, kernel_size=1, bias=False)
+        elif self.skip == 'm2':
+            self.bot_fine = nn.Conv2d(512, self.skip_num, kernel_size=1, bias=False)
+        else:
+            raise Exception('Not a valid skip')
+
+        self.bot_aspp = nn.Conv2d(1280, 256, kernel_size=1, bias=False)
+
+        self.final = nn.Sequential(
+            nn.Conv2d(256 + self.skip_num, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, num_classes, kernel_size=1, bias=False))
+
+        initialize_weights(self.aspp)
+        initialize_weights(self.bot_aspp)
+        initialize_weights(self.bot_fine)
+        initialize_weights(self.final)
+
+    def forward(self, x, gts=None):
+
+        x_size = x.size()  # 800
+        x0 = self.layer0(x)  # 400
+        x1 = self.layer1(x0)  # 400
+        x2 = self.layer2(x1)  # 100
+        x3 = self.layer3(x2)  # 100
+        x4 = self.layer4(x3)  # 100
+        xp = self.aspp(x4)
+
+        dec0_up = self.bot_aspp(xp)
+        if self.skip == 'm1':
+            dec0_fine = self.bot_fine(x1)
+            dec0_up = Upsample(dec0_up, x1.size()[2:])
+        else:
+            dec0_fine = self.bot_fine(x2)
+            dec0_up = Upsample(dec0_up, x2.size()[2:])
+
+        dec0 = [dec0_fine, dec0_up]
+        dec0 = torch.cat(dec0, 1)
+        dec1 = self.final(dec0)
+        main_out = Upsample(dec1, x_size[2:])
+
+        # if self.training:
+        #     return self.criterion(main_out, gts)
+
+        return main_out
+
+
+class DeepWV3Plus(nn.Module):
+    """
+    WideResNet38 version of DeepLabV3
+    mod1
+    pool2
+    mod2 bot_fine
+    pool3
+    mod3-7
+    bot_aspp
+
+    structure: [3, 3, 6, 3, 1, 1]
+    channels = [(128, 128), (256, 256), (512, 512), (512, 1024), (512, 1024, 2048),
+              (1024, 2048, 4096)]
+    """
+
+    def __init__(self, num_classes, trunk='WideResnet38', criterion=None):
+
+        super(DeepWV3Plus, self).__init__()
+        self.criterion = criterion
+        logging.info("Trunk: %s", trunk)
+
+        wide_resnet = wider_resnet38_a2(classes=1000, dilation=True)
+        wide_resnet = torch.nn.DataParallel(wide_resnet)
+        if criterion is not None:
+            try:
+                checkpoint = torch.load('/mnt/lustre/share_data/lixiangtai/wider_resnet38_imagenet.pth', map_location='cpu')
+                wide_resnet.load_state_dict(checkpoint)
+                del checkpoint
+            except:
+                print("Please download the ImageNet weights of WideResNet38 in our repo to ./pretrained_models/wider_resnet38.pth.tar.")
+                raise RuntimeError("=====================Could not load ImageNet weights of WideResNet38 network.=======================")
+        wide_resnet = wide_resnet.module
+
+        self.mod1 = wide_resnet.mod1
+        self.mod2 = wide_resnet.mod2
+        self.mod3 = wide_resnet.mod3
+        self.mod4 = wide_resnet.mod4
+        self.mod5 = wide_resnet.mod5
+        self.mod6 = wide_resnet.mod6
+        self.mod7 = wide_resnet.mod7
+        self.pool2 = wide_resnet.pool2
+        self.pool3 = wide_resnet.pool3
+        del wide_resnet
+
+        self.aspp = _AtrousSpatialPyramidPoolingModule(4096, 256,
+                                                       output_stride=8)
+
+        self.bot_fine = nn.Conv2d(128, 48, kernel_size=1, bias=False)
+        self.bot_aspp = nn.Conv2d(1280, 256, kernel_size=1, bias=False)
+
+        self.final = nn.Sequential(
+            nn.Conv2d(256 + 48, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1, bias=False),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, num_classes, kernel_size=1, bias=False))
+
+        initialize_weights(self.final)
+
+    def forward(self, inp, gts=None):
+
+        x_size = inp.size()
+        x = self.mod1(inp)
+        m2 = self.mod2(self.pool2(x))
+        x = self.mod3(self.pool3(m2))
+        x = self.mod4(x)
+        x = self.mod5(x)
+        x = self.mod6(x)
+        x = self.mod7(x)
+        x = self.aspp(x)
+        dec0_up = self.bot_aspp(x)
+
+        dec0_fine = self.bot_fine(m2)
+        dec0_up = Upsample(dec0_up, m2.size()[2:])
+        dec0 = [dec0_fine, dec0_up]
+        dec0 = torch.cat(dec0, 1)
+
+        dec1 = self.final(dec0)
+        out = Upsample(dec1, x_size[2:])
+
+        # if self.training:
+        #     return self.criterion(out, gts)
+
+        return out
+
+
+
+def DeepSRNX50V3PlusD_m1(num_classes, criterion):
+    """
+    SEResNeXt-50 Based Network
+    """
+    return DeepV3Plus(num_classes, trunk='seresnext-50', criterion=criterion, variant='D',
+                      skip='m1')
+
+
+def DeepR50V3PlusD_m1(num_classes, criterion):
+    """
+    ResNet-50 Based Network
+    """
+    return DeepV3Plus(num_classes, trunk='resnet-50', criterion=criterion, variant='D', skip='m1')
+
+
+def DeepR101V3PlusD_m1(num_classes, criterion):
+    """
+    ResNet-50 Based Network
+    """
+    return DeepV3Plus(num_classes, trunk='resnet-101', criterion=criterion, variant='D', skip='m1')
+
+
+def DeepR50V3PlusD_m1_deeply(num_classes, criterion):
+    """
+    ResNet-50 Based Network
+    """
+    return DeepV3Plus(num_classes, trunk='resnet-50-deep', criterion=criterion, variant='D', skip='m1')
+
+
+def DeepR101V3PlusD_m1_deeply(num_classes, criterion):
+    """
+    ResNet-50 Based Network
+    """
+    return DeepV3Plus(num_classes, trunk='resnet-101-deep', criterion=criterion, variant='D', skip='m1')
+
+
+def DeepR50FCN_m1_deeply(num_classes, criterion):
+    """
+    ResNet-50 Based Network FCN with low-level features
+    """
+    return DeepFCN(num_classes, trunk='resnet-50-deep', criterion=criterion, variant='D', skip='m1')
+
+
+if __name__ == '__main__':
+    # model = DeepWV3Plus(19)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    img = torch.randn(3, 3, 256, 256).to(device)
+    model = DeepWV3Plus(num_classes=19).to(device)
+    out = model(img)
+    print(out.shape)
+    # print(model)
